b907f194764493be59e3e2653c90eeb60175468a
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-07-06 12:20:46 +0200

    Further slide renumbering scripts.

---
+++ courses/1day-202310XX/process_slides_CPE.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-1day-202310XX-02-CPE"
+
+slidealias=( \
+    "Title" \
+    "WhyKnow" \
+    "OperatingSystem" \
+    "ProgrammingModels" \
+    "DevelopmentEnvironment" \
+    "CrayComplingEnv" \
+    "ScientificLibraries" \
+    "CrayMPI" \
+    "Lmod" \
+    "CompilerWrappers" \
+    "SelectingCPEVersion" \
+    "TargetModules" \
+    "PrgEnvCompilerModules" \
+    "GettingHelp" \
+    "ChatGPT" \
+    "OtherModules" \
+    "WarningLibraryPath" \
+    "WarningOrderMatters" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    echo "Moving Dia${number}.png to ${name}.png"
+    /bin/mv -f "Dia${number}.png" "${name}.png"
+done
+
