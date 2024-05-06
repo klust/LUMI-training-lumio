f6dd1d893f5e81cb17d1e6a1d603fc75ea0e22c6
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-05-06 21:40:12 +0200

    Further corrections to the video copy script.

--- courses/4day-20240423/video_4day_20240423.sh
+++ courses/4day-20240423/video_4day_20240423.sh
@@ -84,16 +84,17 @@ copy_to_repo private 1_05_Compilers_and_Parallel_Programming_Models
 copy_to_repo private 1_07_Cray_Scientific_Libraries
 copy_to_repo private 1_09_Offload_CCE
 
-copy_to_repo private 2_05_Advanced_Application_Placement
 copy_to_repo private 2_03_Debugging_at_Scale
+copy_to_repo private 2_05_Advanced_Application_Placement
 
 copy_to_repo private 3_01_Introduction_to_Perftools
 copy_to_repo private 3_03_Advanced_Performance_Analysis
 copy_to_repo private 3_05_Cray_MPI_on_Slingshot
 
 copy_to_repo private 4_01_Introduction_to_Python_on_Cray_EX
-copy_to_repo private 4_02_Performance_Optimization_Improving_Single_Core
-copy_to_repo private 4_04_IO_Optimization_Parallel_IO
+copy_to_repo private 4_02_Porting_to_GPU
+copy_to_repo private 4_03_Performance_Optimization_Improving_Single_Core
+copy_to_repo private 4_05_IO_Optimization_Parallel_IO
 
 #
 # AMD stuff
@@ -103,9 +104,9 @@ echo -e "\nProcessing AMD materials..."
 copy_to_repo public 2_01_Introduction_to_AMD_ROCm_Ecosystem
 copy_to_repo public 3_07_AMD_ROCgdb_Debugger
 copy_to_repo public 3_09_Introduction_to_Rocprof_Profiling_Tool
-copy_to_repo public 4_06_AMD_Omnitrace
-copy_to_repo public 4_08_AMD_Omniperf
-copy_to_repo public 4_10_Best_Practices_GPU_Optimization
+copy_to_repo public 4_07_AMD_Omnitrace
+copy_to_repo public 4_09_AMD_Omniperf
+copy_to_repo public 4_11_Best_Practices_GPU_Optimization
 
 #
 # LUST stuff
@@ -114,4 +115,4 @@ copy_to_repo public 4_10_Best_Practices_GPU_Optimization
 echo -e "\nProcessing LUST materials..."
 copy_to_repo public 1_00_Introduction
 copy_to_repo public 2_07_LUMI_Software_Stacks
-copy_to_repo public 4_11_LUMI_Support_and_Documentation
+copy_to_repo public 4_12_LUMI_Support_and_Documentation
