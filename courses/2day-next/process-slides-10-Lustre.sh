b76044d0025dc15fefc99b3687f54b6ea7c2855a
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-05-13 14:06:30 +0200

    Slightly improved output for the slide processing scripts.

--- courses/2day-next/process-slides-10-Lustre.sh
+++ courses/2day-next/process-slides-10-Lustre.sh
@@ -33,6 +33,8 @@ if [[ "$training" == "$PWD" ]]
 then
     echo "Failed to find the name of the course."
     exit
+else
+    echo "Processing slides in public/$training/img/$slidestack..."
 fi
 
 #
