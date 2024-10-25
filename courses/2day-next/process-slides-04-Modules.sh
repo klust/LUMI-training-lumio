cfe999c2f90a8b8d18205b5059e0059bfd48377c
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-10-25 15:43:45 +0200

    Update of the processing script for the modules section.

--- courses/2day-next/process-slides-04-Modules.sh
+++ courses/2day-next/process-slides-04-Modules.sh
@@ -36,55 +36,17 @@ slidealias=( \
     "ModuleAvail_4" \
     "ModuleAvail_5" \
     "ModuleAvail_6" \
+    "ModuleAvail_7" \
+    "ModuleAvail_8" \
+    "ModuleAvail_9" \
+    "ModuleAvail_10" \
+    "ModuleAvail_11" \
+    "ModuleAvail_12" \
     "ChangingDisplayStyle" \
     "GettingHelp" \
     "NoteCaching" \
     "NoteOtherCommands" \
     "Questions" \
-)
-
-#
-# Not really needed, we'll just check if an ImgXX file exists before
-# doing the move.
-#
-imgalias=( \
-    "None" \
-    "None" \
-    "None" \
-    "None" \
-    "BenefitsHierarchyDemo" \
-    "None" \
-    "None" \
-    "None" \
-    "ModuleSpiderCommand_1" \
-    "ModuleSpiderCommand_2" \
-    "ModuleSpiderCommand_3" \
-    "ModuleSpiderFFTW" \
-    "ModuleSpiderFFTWVersion_1" \
-    "ModuleSpiderFFTWVersion_2" \
-    "None" \
-    "ModuleSpiderGnuplot_1" \
-    "ModuleSpiderGnuplot_2" \
-    "ModuleSpiderGnuplotVersion_1" \
-    "ModuleSpiderGnuplotVersion_2" \
-    "None" \
-    "ModuleSpiderCMake" \
-    "ModuleSpiderCMakeVersion_1" \
-    "ModuleSpiderCMakeVersion_2" \
-    "None" \
-    "ModuleKeywordHTTPS_1" \
-    "ModuleKeywordHTTPS_2" \
-    "None" \
-    "ModuleAvail_1" \
-    "ModuleAvail_2" \
-    "ModuleAvail_3" \
-    "ModuleAvail_4" \
-    "ModuleAvail_5" \
-    "ModuleAvail_6" \
-    "ChangingDisplayStyle" \
-    "GettingHelp" \
-    "NoteCaching" \
-    "NoteOtherCommands" \
 )
 
 #
