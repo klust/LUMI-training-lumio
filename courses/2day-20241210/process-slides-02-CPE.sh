ee493812c8c48d47f3b45307d1ae736cc92e94b7
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-11-13 15:00:32 +0100

    LUMI-O management scripts for the 20241210 2-day course.

---
+++ courses/2day-20241210/process-slides-02-CPE.sh
@@ -0,0 +1,56 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-2day-20241210-02-CPE"
+
+slidealias=( \
+    "Title" \
+    "WhyKnow" \
+    "OperatingSystem" \
+    "LowNoiseMode" \
+    "ProgrammingModels" \
+    "DevelopmentEnvironment" \
+    "CrayComplingEnv" \
+    "ScientificLibraries" \
+    "CrayMPI" \
+    "CrayMPIGPU" \
+    "Lmod" \
+    "CompilerWrappers" \
+    "SelectingCPEVersion" \
+    "TargetModules" \
+    "PrgEnvCompilerModules" \
+    "GettingHelp" \
+    "ChatGPT" \
+    "OtherModules" \
+    "WarningLibraryPath" \
+    "WarningOrderMatters" \
+    "NoteWrappers" \
+    "Questions" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+else
+    echo "Processing slides in public/$training/img/$slidestack..."
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    echo "Moving Dia${number}.png to ${name}.png"
+    /bin/mv -f "Dia${number}.png" "${name}.png"
+done
+
---
+++ courses/2day-20241210/process-slides-07-Binding.sh
@@ -0,0 +1,70 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-2day-20241210-07-Binding"
+
+slidealias=( \
+    "Title" \
+    "WhatAbout" \
+    "WhenDone" \
+    "WhyNeedThis" \
+    "CoreNumbering" \
+    "GPUNumbering_1" \
+    "GPUNumbering_2" \
+    "GPUNumberingRemarks" \
+    "SlurmTaskDistribution_1" \
+    "SlurmTaskDistribution_2" \
+    "SlurmTaskCPU" \
+    "SlurmTaskCPUMasks" \
+    "SlurmTaskCPUExamples" \
+    "SlurmTaskGPU" \
+    "MPICHRankRedistribution" \
+    "OpenMPBinding" \
+    "OpenMPBindingPlaces" \
+    "OpenMPBindingProcBind" \
+    "ROCRGPU" \
+    "ROCRGPUMap_1" \
+    "ROCRGPUMap_2" \
+    "ROCRGPURing" \
+    "ROCRMechanism" \
+    "ROCRMechanismLinearGCD1" \
+    "ROCRMechanismLinearGCD2" \
+    "ROCRMechanismLinearCCD1" \
+    "ROCRMechanismLinearCCD2" \
+    "ROCRMechanismLinearCCD3" \
+    "ROCRMechanismGreenRing1" \
+    "ROCRMechanismGreenRing2" \
+    "ROCRMechanismGreenRing3" \
+    "ROCRMechanismAllocateByResource" \
+    "Questions" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+else
+    echo "Processing slides in public/$training/img/$slidestack..."
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    if [ -f "Dia$number.png" ]
+    then
+        echo "Moving Dia${number}.png to ${name}.png"
+        /bin/mv -f "Dia${number}.png" "${name}.png"
+    fi
+done
+
