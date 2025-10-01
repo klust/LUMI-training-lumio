f10650c6eafa1eb8fce4a721b87bb2db4eadc535
Kurt Lust <Kurt.Lust@uantwerpen.be>
2025-10-01 11:24:34 +0200

    First version of the processing scripts for the Tallinn intro training in October 2025.

---
+++ courses/2day-20251020/process-slides-102-CPE.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-2day-20251020-102-CPE"
+
+slidealias=( \
+    "Title" \
+    "WhyKnow" \
+    "OperatingSystem" \
+    "LowNoiseMode" \
+    "ProgrammingModels" \
+    "DevelopmentEnvironment" \
+    "AMDTools" \
+    "CrayCompilingEnv" \
+    "ScientificLibraries" \
+    "CrayMPI" \
+    "CrayMPIGPU" \
+    "Lmod" \
+    "CompilerWrappers" \
+    "SelectingCPEVersion" \
+    "TargetModules" \
+    "PrgEnvCompilerModules" \
+    "GettingHelp" \
+    "ChatGPT" \
+    "OtherModules" \
+    "WarningLibraryPath" \
+    "WarningOrderMatters" \
+    "NoteWrappers" \
+    "Questions" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+else
+    echo "Processing slides in public/$training/img/$slidestack..."
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    echo "Moving Dia${number}.png to ${name}.png"
+    /bin/mv -f "Dia${number}.png" "${name}.png"
+done
+
---
+++ courses/2day-20251020/process-slides-203-Lustre.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+# Some variables.
+slidestack="LUMI-2day-20251020-203-Lustre"
+
+slidealias=( \
+    "Title" \
+    "FileSystemLumi" \
+    "LustreBuildingBlocks1" \
+    "LustreBuildingBlocks2" \
+    "LustreFileStriping" \
+    "LustreFileAccess" \
+    "LustreParallelismKey_1" \
+    "LustreParallelismKey_2" \
+    "LustreDetermineParameters" \
+    "LustreManageStriping1" \
+    "LustreManageStriping2" \
+    "LustreManageStriping3" \
+    "LustreManageStriping4" \
+    "LustreMDS_1" \
+    "LustreMDS_2" \
+    "LumiLustreOnLumi" \
+    "Questions" \
+)
+
+#
+# Automatically set variables
+#
+training="${PWD##*/LUMI-training-lumio/courses/}"
+if [[ "$training" == "$PWD" ]]
+then
+    echo "Failed to find the name of the course."
+    exit
+else
+    echo "Processing slides in public/$training/img/$slidestack..."
+fi
+
+#
+# Do the actual work of renaming the slides
+#
+
+cd "../../public/$training/img/$slidestack"
+
+number=0
+for name in ${slidealias[@]}
+do 
+    number=$((number+1))
+    if [ -f "Dia$number.png" ]
+    then
+        echo "Moving Dia${number}.png to ${name}.png"
+        /bin/mv -f "Dia${number}.png" "${name}.png"
+    fi
+done
+
