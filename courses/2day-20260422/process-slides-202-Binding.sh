f505b6f2ec599adcc5235fd182f08bdac0e241fe
Kurt Lust <Kurt.Lust@uantwerpen.be>
2026-04-09 16:13:44 +0200

    Additional slides for the bindings talk.

--- courses/2day-20260422/process-slides-202-Binding.sh
+++ courses/2day-20260422/process-slides-202-Binding.sh
@@ -35,12 +35,19 @@ slidealias=( \
     "ROCRMechanism" \
     "ROCRMechanismLinearGCD1" \
     "ROCRMechanismLinearGCD2" \
+    "ROCRMechanismLinearGCD3" \
+    "ROCRMechanismLinearGCD4" \
     "ROCRMechanismLinearCCD1" \
     "ROCRMechanismLinearCCD2" \
     "ROCRMechanismLinearCCD3" \
+    "ROCRMechanismLinearCCD4" \
+    "ROCRMechanismLinearCCD5" \
+    "ROCRMechanismLinearCCD6" \
     "ROCRMechanismGreenRing1" \
     "ROCRMechanismGreenRing2" \
     "ROCRMechanismGreenRing3" \
+    "ROCRMechanismGreenRing4" \
+    "ROCRMechanismGreenRing5" \
     "ROCRMechanismAllocateByResource" \
     "Questions" \
 )
